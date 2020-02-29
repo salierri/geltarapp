92da3dbe09dd25c451130c1bd7971f0af9f20f14
Gulyás Gergely <g.gergely314@gmail.com>
2020-02-29 13:58:27 +0100

    Refactor video player

--- client/src/components/videoPlayer.ts
+++ client/src/components/videoPlayer.ts
@@ -6,31 +6,25 @@ interface Players {
 let players: Players = { };
 let autoplay: number = 0;
 
+function createPlayer(elementId: string, video: string) {
+    return new YT.Player(elementId, {
+        height: '243',
+        width: '400',
+        videoId: video,
+        playerVars: {
+            controls: 0,
+            autoplay: autoplay,
+            loop: 1
+        },
+        events: {
+          'onReady': onPlayerReady
+        }
+    });
+}
+
 export function APIReady() {
-    players.music = new YT.Player('musicPlayer', {
-        height: '243',
-        width: '400',
-        videoId: 'm_8QMAChwtg',
-        playerVars: {
-            controls: 0,
-            autoplay: autoplay
-        },
-        events: {
-          'onReady': onPlayerReady
-        }
-    });
-    players.ambience = new YT.Player('ambiencePlayer', {
-        height: '243',
-        width: '400',
-        videoId: 'sGkh1W5cbH4',
-        playerVars: {
-            controls: 0,
-            autoplay: autoplay
-        },
-        events: {
-          'onReady': onPlayerReady
-        }
-    });
+    players.music = createPlayer('musicPlayer', 'm_8QMAChwtg');
+    players.ambience = createPlayer('ambiencePlayer', 'sGkh1W5cbH4');
 }
 
 function onPlayerReady(event: YT.PlayerEvent) {
