06e9b8137215bb055dd9f81c299edc2fe5d623d1
Gulyás Gergely <g.gergely314@gmail.com>
2021-03-29 00:32:08 +0200

    Cookie test on staging environment

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -8,14 +8,22 @@ const router = Express.Router();
 router.post('/:roomId', async (req, res) => {
   const roomId = req.params.roomId;
   const room = await Room.findOne({ _id: roomId });
-  const pass: string = req.body.password
-  const isMaster: boolean = await bcrypt.compare(req.body.password, room?.masterPassword ?? '');
-  const isUser: boolean = await bcrypt.compare(req.body.password, room?.password ?? '');
+  const pass: string = req.body.password;
+  const isMaster: boolean = await bcrypt.compare(pass, room?.masterPassword ?? '');
+  const isUser: boolean = await bcrypt.compare(pass, room?.password ?? '');
   if (isMaster) {
     const newSession = await new Session({ room: roomId, master: true }).save();
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Cookie');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.cookie('X-Server-Auth-Token', newSession._id, { sameSite: 'lax', maxAge: 259200000, httpOnly: true });
     res.send(JSON.stringify({ room: roomId, session: newSession._id, master: true }));
   } else if (isUser) {
     const newSession = await new Session({ room: roomId, master: false }).save();
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Cookie');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.cookie('X-Server-Auth-Token', newSession._id, { sameSite: 'lax', maxAge: 259200000, httpOnly: true });
     res.send(JSON.stringify({ room: roomId, session: newSession._id , master: false }));
   } else {
     res.sendStatus(401);
