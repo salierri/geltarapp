a89a56ae5bd4524c72ca067e610944d6587a94c9
Gulyás Gergely <g.gergely314@gmail.com>
2021-03-30 18:44:04 +0200

    Remove the server-side cookie setting

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -13,17 +13,9 @@ router.post('/:roomId', async (req, res) => {
   const isUser: boolean = await bcrypt.compare(pass, room?.password ?? '');
   if (isMaster) {
     const newSession = await new Session({ room: roomId, master: true }).save();
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Cookie');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.cookie('X-Server-Auth-Token', newSession._id, { sameSite: 'lax', maxAge: 259200000, httpOnly: true });
     res.send(JSON.stringify({ room: roomId, session: newSession._id, master: true }));
   } else if (isUser) {
     const newSession = await new Session({ room: roomId, master: false }).save();
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Cookie');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.cookie('X-Server-Auth-Token', newSession._id, { sameSite: 'lax', maxAge: 259200000, httpOnly: true });
     res.send(JSON.stringify({ room: roomId, session: newSession._id , master: false }));
   } else {
     res.sendStatus(401);
